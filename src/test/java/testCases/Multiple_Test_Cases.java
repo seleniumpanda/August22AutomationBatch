f7ec4587046e2df238d33a5ebbee5a2129c19df5
seleniumpanda <seleniumpanda@gmail.com>
2022-09-22 22:20:33 -0400

    added steps of how to use one DataProvider with multiple test Cases

--- src/test/java/testCases/Multiple_Test_Cases.java
+++ src/test/java/testCases/Multiple_Test_Cases.java
@@ -5,6 +5,18 @@ import org.testng.annotations.Test;
 import dataProvider_MultipleTestCases.DataRepository;
 
 public class Multiple_Test_Cases {
+	
+	/* 1 test case - 1 DataProvider
+
+	50 test cases - 1 DataProvider ??
+
+	1. I will create a separate package > A class > @DataProvider
+	2. I will create another package where in I will write all my Test Cases
+	3. The @DataProvider's method I have to make it static - this is to connect with the other Classes
+	4. Linking process @Test(dataProviderClass = ClassName.class) 
+	5. Again in the test cases I have to pass the (dataProvider = "nameoftheMethodInsidetheDataProvider")
+	6. In all the test cases have to pass the input parameters == no of cols
+	7. pass Method (java.lang.reflect) as an input parameter inside the @DataProvider methodname (parameterize) */
 
 	
 	@Test(dataProvider = "getData", dataProviderClass = DataRepository.class)
